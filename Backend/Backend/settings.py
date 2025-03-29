e700e2633380dfa373de2b9a56898399c0473e45
affilpm <affilpm2004@gmail.com>
2025-03-29 13:21:42 +0530

    updated docker-compose

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -396,24 +396,3 @@ STATIC_URL = '/static/'
 
 
 
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG',
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    },
-}
