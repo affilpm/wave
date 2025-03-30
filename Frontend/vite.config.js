efdec8ec6ac9ea575c0934315ef942161228808e
affilpm <affilpm2004@gmail.com>
2025-03-31 00:43:19 +0530

    updated vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/ws/webrtc': {
-        target: 'ws://localhost:8000',
+        target: 'ws://13.49.227.70:8000',
         ws: true,
         changeOrigin: true,
       },
@@ -25,7 +25,7 @@ export default defineConfig({
         frame-src 'self' https://accounts.google.com https://*.gstatic.com https://*.razorpay.com;
         frame-ancestors 'self' https://accounts.google.com https://*.gstatic.com;
         connect-src 'self' 
-          ws://localhost:8000 ws://localhost:* wss://localhost:* http://localhost:* https://localhost:* wss://*.razorpay.com
+          ws://13.49.227.70:8000 ws://13.49.227.70:* wss://13.49.227.70:* http://13.49.227.70:* https://13.49.227.70:* wss://*.razorpay.com
           wss://*.agora.io wss://*.sd-rtn.com
           wss://*.edge.agora.io wss://*.edge.sd-rtn.com
           https://*.agora.io https://*.sd-rtn.com
@@ -35,8 +35,8 @@ export default defineConfig({
           wss://107-155-41-35.edge.sd-rtn.com:*
           wss://*.edge.agora.io:* 
           wss://*.edge.sd-rtn.com:*;
-        img-src 'self' http://localhost:* https: data: blob:;
-        media-src 'self' blob: data: http://localhost:* https://localhost:*;
+        img-src 'self' http://13.49.227.70:* https: data: blob:;
+        media-src 'self' blob: data: http://13.49.227.70:* https://13.49.227.70:*;
         font-src 'self' data:;
         worker-src 'self' blob:;
       `.replace(/\s+/g, ' ').trim(),
