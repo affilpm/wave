fa0831e3a5e23676cf63084f726ce60340fb727d
affilpm <affilpm2004@gmail.com>
2025-04-07 16:35:03 +0530

    updated vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -25,13 +25,16 @@ export default defineConfig({
         frame-src 'self' https://accounts.google.com https://*.gstatic.com https://*.razorpay.com;
         frame-ancestors 'self' https://accounts.google.com https://*.gstatic.com;
         connect-src 'self'
+          ws://localhost:8000 ws://localhost:* wss://localhost:* http://localhost:* https://localhost:*
           https://api.affils.site wss://api.affils.site
           wss://*.razorpay.com
           wss://*.agora.io wss://*.sd-rtn.com
           wss://*.edge.agora.io wss://*.edge.sd-rtn.com
           https://*.agora.io https://*.sd-rtn.com
           https://web-2.statscollector.sd-rtn.com 
-          https://statscollector-1.agora.io;
+          https://statscollector-1.agora.io
+          wss://107-155-41-35.edge.agora.io:* wss://107-155-41-35.edge.sd-rtn.com:*
+          wss://*.edge.agora.io:* wss://*.edge.sd-rtn.com:*;
         img-src 'self' https: data: blob:;
         media-src 'self' blob: data: https://api.affils.site;
         font-src 'self' data:;
